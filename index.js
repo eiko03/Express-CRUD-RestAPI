5823cc56a3a51cf865a9dfdff79de999cb8ee724
Tipu Sultan Eiko <tipusultan50@gmail.com>
2020-08-08 20:03:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
+
 const logger = require('./middleware/logger');
 const members = require('./Members');
 
@@ -9,9 +9,7 @@ const app = express();
 // Init middleware
 // app.use(logger);
 
-// Handlebars Middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+
 
 // Body Parser Middleware
 app.use(express.json());
